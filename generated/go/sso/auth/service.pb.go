40482e89b73b097c79775146a13c61ee1b579d1b
a.sadikov <a.sadikov@qlean.ru>
2024-07-29 21:10:06 +0300

    fixed proto

--- generated/go/sso/auth/service.pb.go
+++ generated/go/sso/auth/service.pb.go
@@ -7,7 +7,7 @@
 package auth
 
 import (
-	user "cinematic.back/sso/user"
+	user "cinematic.com/sso/user"
 	protoreflect "google.golang.org/protobuf/reflect/protoreflect"
 	protoimpl "google.golang.org/protobuf/runtime/protoimpl"
 	reflect "reflect"
